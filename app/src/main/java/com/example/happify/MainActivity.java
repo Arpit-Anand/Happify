15e2a0be9bec68f9f549aa9ed9616dc7ce01fb87
Arpit-Anand <lostbullet1107@gmail.com>
2021-06-20 22:32:36 +0530

    mostly first page

--- app/src/main/java/com/example/happify/MainActivity.java
+++ app/src/main/java/com/example/happify/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.happify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button close = findViewById(R.id.close);
+        close.setActivated(true);
+    }
+    public void test(View v){
+        Intent i = new Intent(this, Test.class);
+        startActivity(i);
+    }
+    public void breathe(View v){
+        Intent i = new Intent(this,Breathe.class);
+        startActivity(i);
+    }
+    public void read(View v){
+        Intent i = new Intent(this, Read.class);
+        startActivity(i);
+    }
+    public void close(View v){
+        MainActivity.this.finish();
+        System.exit(0);
     }
 }
